39de79bf3ab18678c30efa5ab6cad7b1e4c679c4
Eduardo Julian <eduardoejp@gmail.com>
2016-08-07 16:45:49 -0400

    - Added project.clj file.

---
+++ project.clj
@@ -0,0 +1,20 @@
+(defproject com.github.luxlang/frame-vertx "0.1.0-SNAPSHOT"
+  :description "A Vert.x driver for Frame."
+  :url "https://github.com/LuxLang/frame-vertx"
+  :license {:name "Mozilla Public License (Version 2.0)"
+            :url "https://www.mozilla.org/en-US/MPL/2.0/"}
+  :plugins [[com.github.luxlang/lein-luxc "0.3.0-SNAPSHOT"]]
+  :dependencies [[com.github.luxlang/lux-stdlib "0.4.0-SNAPSHOT"]
+                 [com.github.luxlang/frame "0.1.0-SNAPSHOT"]
+                 [io.vertx/vertx-web "3.2.0"]]
+  :source-paths ["source"]
+  :repositories [["snapshots" "https://oss.sonatype.org/content/repositories/snapshots/"]
+                 ["releases" "https://oss.sonatype.org/service/local/staging/deploy/maven2/"]]
+  :deploy-repositories [["releases" {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
+                                     :creds :gpg}]
+                        ["snapshots" {:url "https://oss.sonatype.org/content/repositories/snapshots/"
+                                      :creds :gpg}]]
+  :pom-addition [:developers [:developer {:id "eduardoejp"}
+                              [:name "Eduardo Julian"]
+                              [:url "https://github.com/eduardoejp"]]]
+  )
